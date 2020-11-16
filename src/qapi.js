ad3fc11dfcd073c44409212d4f4130b27f4f0b1c
Shaik Aslam <44738054+Shaikaslam340@users.noreply.github.com>
2020-11-16 23:36:19 +0530

    Initial commit

---
+++ src/qapi.js
@@ -0,0 +1,25 @@
+export default {
+	"pages": {
+		"root": {
+			"id": "root",
+			"pageUrl": "root",
+			"name": "root",
+			"children": [
+				"5fb2bd71cae167001e06a519",
+				"5fb2bd71cae167001e06a51b"
+			]
+		},
+		"5fb2bd71cae167001e06a519": {
+			"id": "5fb2bd71cae167001e06a519",
+			"name": "404",
+			"pageUrl": "404"
+		},
+		"5fb2bd71cae167001e06a51b": {
+			"id": "5fb2bd71cae167001e06a51b",
+			"name": "index",
+			"pageUrl": "index"
+		}
+	},
+	"mode": "production",
+	"projectType": "create-react-app"
+}
